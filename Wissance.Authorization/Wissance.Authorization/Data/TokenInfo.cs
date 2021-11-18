8c5edc594c8517a1f480a3ec3ed9bd4a5bc59f4b
Ushakov Michael <um.nix.user@gmail.com>
2021-11-18 11:53:12 +0500

    added data

---
+++ Wissance.Authorization/Wissance.Authorization/Data/TokenInfo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Wissance.Authorization.Data
+{
+    public class TokenInfo
+    {
+        public TokenInfo()
+        {
+        }
+
+        public TokenInfo(string session, string accessToken, int accessTokenExpiration, 
+                         string refreshToken, int refreshTokenExpiration)
+        {
+            Session = session;
+            AccessToken = accessToken;
+            AccessTokenExpiration = accessTokenExpiration;
+            RefreshToken = refreshToken;
+            RefreshTokenExpiration = refreshTokenExpiration;
+        }
+
+        public string Session { get; set; }
+        public string AccessToken { get; set; }
+        public int AccessTokenExpiration { get; set; }
+        public string RefreshToken { get; set; }
+        public int RefreshTokenExpiration { get; set; }
+    }
+}
---
+++ Wissance.Authorization/Wissance.Authorization/Data/UserInfo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Wissance.Authorization.Data
+{
+    public class UserInfo
+    {
+        public UserInfo()
+        {
+        }
+
+        public UserInfo(string session, string userName, string[] roles)
+        {
+            Session = session;
+            UserName = userName;
+            Roles = roles;
+        }
+
+        public string Session { get; set; }
+        public string UserName { get; set; }
+        public string[] Roles { get; set; } 
+    }
+}

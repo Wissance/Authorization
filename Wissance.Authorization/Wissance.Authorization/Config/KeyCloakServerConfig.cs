73d0993e336749c55ecb6913a1e8f515fa10d985
Ushakov Michael <um.nix.user@gmail.com>
2021-11-18 16:50:45 +0500

    Basic impl of token get logic

---
+++ Wissance.Authorization/Wissance.Authorization/Config/KeyCloakServerConfig.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Wissance.Authorization.Config
+{
+    public class KeyCloakServerConfig
+    {
+        public KeyCloakServerConfig()
+        {
+
+        }
+
+        public KeyCloakServerConfig(string baseUri, string realm, string clientId, string clientSecret)
+        {
+            BaseUrl = baseUri;
+            Realm = realm;
+            ClientId = clientId;
+            ClientSecret = clientSecret;
+        }
+
+        public string BaseUrl { get; set; }
+        public string Realm { get; set; }
+        public string ClientId { get; set; }
+        public string ClientSecret { get; set; }
+    }
+}
--- Wissance.Authorization/Wissance.Authorization/OpenId/KeyCloakOpenIdAuthenticator.cs
+++ Wissance.Authorization/Wissance.Authorization/OpenId/KeyCloakOpenIdAuthenticator.cs
@@ -1,12 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Net.Http;
 using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Wissance.Authorization.Config;
 using Wissance.Authorization.Data;
+using Wissance.Authorization.Dto.KeyCloak;
 
 namespace Wissance.Authorization.OpenId
 {
     public class KeyCloakOpenIdAuthenticator : IOpenIdAuthenticator
     {
+        public KeyCloakOpenIdAuthenticator(KeyCloakServerConfig config, ILoggerFactory loggerFactory)
+        {
+            _config = config;
+            _logger = loggerFactory.CreateLogger<KeyCloakOpenIdAuthenticator>();
+        }
+
         public TokenInfo Authenticate(IDictionary<string, string> formParameters)
         {
             throw new NotImplementedException();
@@ -21,5 +33,41 @@ namespace Wissance.Authorization.OpenId
         {
             throw new NotImplementedException();
         }
+
+        private async Task<TokenInfo> GetTokenAsync(string realm, FormUrlEncodedContent formContent, string operation)
+        {
+            try
+            {
+                string url = KeyCloakUriHelper.GetTokenUri(_config.BaseUrl, realm);
+                using (HttpClient httpClient = new HttpClient())
+                {
+                    HttpResponseMessage response = await httpClient.PostAsync(url, formContent);
+                    string responseBody = await response.Content.ReadAsStringAsync();
+                    if (response.StatusCode != System.Net.HttpStatusCode.OK)
+                    {
+                        _logger.LogDebug($"Can't {operation}: {response.StatusCode} with body: {responseBody}");
+                        return null;
+                    }
+
+                    TokenDto tokenData = JsonConvert.DeserializeObject<TokenDto>(responseBody);
+                    if (tokenData == null)
+                    {
+                        _logger.LogError("Keycloak token is null (can't be deserialized)");
+                        return null;
+                    }
+
+                    return new TokenInfo(tokenData.SessionState, tokenData.AccessToken, tokenData.TokenExpiration, 
+                                         tokenData.RefreshToken, tokenData.RefreshExpiration);
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"An error occurred during {operation} from: {_config.BaseUrl}, error: {ex.Message}");
+                return null;
+            }
+        }
+
+        private readonly KeyCloakServerConfig _config;
+        private readonly ILogger<KeyCloakOpenIdAuthenticator> _logger;
     }
 }

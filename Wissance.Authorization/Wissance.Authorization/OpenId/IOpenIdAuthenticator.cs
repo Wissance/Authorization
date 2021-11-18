52906549346c7ab3ad00f25ffccd5bd34007031c
Ushakov Michael <um.nix.user@gmail.com>
2021-11-18 11:56:56 +0500

    added preliminary OpenId authenticator interface

---
+++ Wissance.Authorization/Wissance.Authorization/OpenId/IOpenIdAuthenticator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Wissance.Authorization.Data;
+
+namespace Wissance.Authorization.OpenId
+{
+    public interface IOpenIdAuthenticator
+    {
+        TokenInfo Authenticate(string baseUrl, IDictionary<string, string> formParameters);
+        UserInfo GetUserInfo(string baseUrl);
+    }
+}
---
+++ Wissance.Authorization/Wissance.Authorization/OpenId/KeyCloakOpenIdAuthenticator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Wissance.Authorization.Data;
+
+namespace Wissance.Authorization.OpenId
+{
+    public class KeyCloakOpenIdAuthenticator : IOpenIdAuthenticator
+    {
+        public TokenInfo Authenticate(string baseUrl, IDictionary<string, string> formParameters)
+        {
+            throw new NotImplementedException();
+        }
+
+        public UserInfo GetUserInfo(string baseUrl)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

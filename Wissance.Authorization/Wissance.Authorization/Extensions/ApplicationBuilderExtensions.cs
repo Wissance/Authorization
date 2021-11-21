d1a3a99142dc8afd9c09545cc29fe7f18dc96793
Ushakov Michael <um.nix.user@gmail.com>
2021-11-21 12:50:14 +0500

    add IApplicationBuilder method 4 working with security

--- Wissance.Authorization/Wissance.Authorization/Extensions/ApplicationBuilderExtensions.cs
+++ Wissance.Authorization/Wissance.Authorization/Extensions/ApplicationBuilderExtensions.cs
@@ -2,13 +2,25 @@
 using System.Collections.Generic;
 using System.Text;
 using Microsoft.AspNetCore.Builder;
+using Wissance.Authorization.Config;
 
 namespace Wissance.Authorization.Extensions
 {
-    public static class IApplicationBuilderExtensions
+    public static class ApplicationBuilderExtensions
     {
-        public static void UseSwaggerWithAuthentication(this IApplicationBuilder builder)
+        public static void UseSwaggerWithKeyCloakAuthentication(this IApplicationBuilder app, string appName, 
+                                                                KeyCloakServerConfig config, string[] scopesOptions)
         {
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", appName);
+                c.OAuthClientId(config.ClientId);
+                //c.OAuthScopes(new string[] { "profile" });
+                c.OAuthScopes(scopesOptions);
+                c.OAuthRealm(config.Realm);
+                c.OAuthUsePkce();
+            });
         }
     }
 }
